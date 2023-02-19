65df94975bb6b41dc535a8c3555ef4d1e63556f3
harinder24 <hss022002@gmail.com>
2023-02-18 21:41:08 -0800

    done

--- app/src/main/java/com/example/app/MainActivity.kt
+++ app/src/main/java/com/example/app/MainActivity.kt
@@ -3,13 +3,21 @@ package com.example.app
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.app.ui.theme.AppTheme
 
 class MainActivity : ComponentActivity() {
@@ -22,7 +30,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-                    Greeting("Android")
+                    LoadUi("a","b")
                 }
             }
         }
@@ -30,14 +38,34 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+fun LoadUi(text1: String, text2: String){
+
+    Column(modifier = Modifier
+        .fillMaxWidth()
+        .fillMaxHeight(),
+    horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+
+    )
+    {
+        val image = painterResource(id = R.drawable.ic_task_completed)
+        Image(painter = image, contentDescription = null)
+        ShowingText(text1 = text1, text2 = text2)
+
+    }
+}
+
+@Composable
+fun ShowingText(text1: String, text2: String) {
+
+    Text(text = text1, fontWeight = FontWeight.Bold, modifier = Modifier.padding(top = 24.dp, bottom = 8.dp))
+    Text(text = text2, fontSize = 16.sp)
 }
 
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
     AppTheme {
-        Greeting("Android")
+        LoadUi(text1 = stringResource(R.string.text1),text2 = stringResource(R.string.text2))
     }
 }
